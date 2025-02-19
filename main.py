95731522272997bd79d3b576bc207442113faecb
futurecodes <15356303+futurecodes@user.noreply.gitee.com>
2025-02-19 23:52:06 +0800

    refactor(main): 重构主程序并添加距离度量配置
    - 移除了未使用的导入和注释
    - 更新了 FaceDatasetConfig 的导入路径
    - 添加了 metrics_config 的具体参数
    - 实例化了 data 和 metrics 对象

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
 import tyro 
 from experiment.base_experiment import ExperimentConfig 
-from data.base_dataset import FaceDatasetConfig
+from data.face_dataset import FaceDatasetConfig 
 from method.bio_hash import BioHashConfig
 
 from metrics.performance.performance_metrics import EERMetricsConfig,EERMetrics
 import numpy as np 
 from config.base_config import BaseConfig
 
-# 数据
-
-
-# 方法 
-
-
-
-arr_input = np.random.rand(10,)
-
-output = method.process_feature(arr_input)
 
 
 # 建立配置类，确定参数 
 method_config = BioHashConfig()
 data_config =  FaceDatasetConfig(
-    data_dir = "./embeddings/LFW",
-)
-metrics_config = EERMetricsConfig() 
+                    dataset_name="LFW",)
+metrics_config = EERMetricsConfig(measure="euclidean")  
 
 # 实例化
 method = method_config.setup()
+data = data_config.setup()
+metrics = metrics_config.setup()
