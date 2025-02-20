9cb9b881c01fe8ace9b7e66a25928d65da9f4169
futurecodes <15356303+futurecodes@user.noreply.gitee.com>
2025-02-20 11:25:44 +0800

    refactor(project): 重构项目文件结构和配置
    - 修改了数据配置类的名称，使用 lfw_data_config 替代 data_config
    - 将 sliding_window.py 移动到 method/modules 目录下
    - 新增 pytest.ini 文件，配置测试路径
    - 删除了 test/metrics/performance/test_performance_metrics.py 文件

--- main.py
+++ main.py
@@ -11,11 +11,15 @@ from config.base_config import BaseConfig
 
 # 建立配置类，确定参数 
 method_config = BioHashConfig()
-data_config =  FaceDatasetConfig(
+lfw_data_config =  FaceDatasetConfig(
                     dataset_name="LFW",)
 metrics_config = EERMetricsConfig(measure="euclidean")  
 
+
 # 实例化
 method = method_config.setup()
-data = data_config.setup()
+data = lfw_data_config.setup()
 metrics = metrics_config.setup()
+# Expriment 
+
+# method data metrics
