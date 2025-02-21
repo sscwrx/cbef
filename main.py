25a7bda6d4b8f3e5441ef1efcbec84dea2b249d7
futurecodes <15356303+futurecodes@user.noreply.gitee.com>
2025-02-21 23:38:51 +0800

    refactor(main): 重构实验配置并测试 SWG 方法

--- main.py
+++ main.py
@@ -7,31 +7,26 @@ from method.bio_hash import BioHashConfig
 from method.swg import SWGConfig
 from method.bi_avet import BiAVETConfig
 from experiment.base_experiment import ExperimentConfig ,Experiment
-from metrics.performance.performance_metrics import EERMetricsConfig,EERMetrics
-
-from rich import print as rprint
-from rich.console import Console
-from rich.panel import Panel
-from rich.table import Table
-console = Console()
+from metrics.performance.eer_metrics import EERMetricsConfig,EERMetrics
 
 if __name__ == "__main__":
 
     # swg_config = SWGConfig()
-    bi_avet_config = BiAVETConfig()
+    # bi_avet_config = BiAVETConfig()
+    swg_config = SWGConfig()
     dataset_config = FaceDatasetConfig(dataset_name="LFW") 
     dataset2_config = FingerprintDatasetConfig(dataset_name="FVC2002/Db1_a")
     eer_config = EERMetricsConfig(measure="hamming")
 
     
     expr_config = ExperimentConfig(
-        expriment_name="测试下指纹数据集",
-        method_config=bi_avet_config,
+        expriment_name="测试下swg是否正常",
+        method_config=swg_config,
         dataset_config=dataset2_config,
         metrics_config=eer_config,
-        expriment_times=1
+        expriment_times=5
     )
-    
+
     experiment:Experiment = expr_config.setup()
 
     experiment.run()
