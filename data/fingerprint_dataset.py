f7a6c9fd3ff817c21ed5f1e1974ead6205584913
futurecodes <15356303+futurecodes@user.noreply.gitee.com>
2025-02-21 21:10:07 +0800

    feat(data): 添加指纹数据集加载功能
    - 在 FingerprintDataset 类中实现了 load_data 方法
    - 该方法会加载指定数据集中的指纹嵌入向量
    - 使用 scipy 和 numpy 处理数据，返回一个字典
    - 字典的键为 (user_id, sample_id) 元组，值为嵌入向量

--- data/fingerprint_dataset.py
+++ data/fingerprint_dataset.py
@@ -1,14 +1,16 @@
 from dataclasses import dataclass, field
-from typing import Type, Literal, Optional
+from typing import Type, Literal, Optional,Dict,Tuple
 from pathlib import Path
 from data.base_dataset import BaseDataset, BaseDatasetConfig
-
+from numpy.typing import NDArray
+import scipy as sp
+import numpy as np 
 @dataclass
 class FingerprintDatasetConfig(BaseDatasetConfig):
     """Configuration class for fingerprint datasets."""
     _target: Type = field(default_factory=lambda: FingerprintDataset)
-    dataset_name: Optional[Literal["FVC2002/Db1_a","FVC2002/Db2_a","FVC2002/Db3_a",
-                                   "FVC2004/Db1_a","FVC2004/Db2_a","FVC2004/Db3_a"]] = None
+    dataset_name: Literal["FVC2002/Db1_a","FVC2002/Db2_a","FVC2002/Db3_a",
+                          "FVC2004/Db1_a","FVC2004/Db2_a","FVC2004/Db3_a"] = "FVC2002/Db1_a"
     n_subjects: int = 100
     samples_per_subject: int = 5
     embedding_dim: int = 299
@@ -21,6 +23,16 @@ class FingerprintDataset(BaseDataset):
         self.config = config
         assert self.config.dataset_name is not None, "Dataset name must be provided."
         assert Path(self.config.embeddings_dir / self.config.dataset_name).exists(), "Embeddings directory does not exist."
-    def load_data(self):
+    def load_data(self)->Dict[Tuple[int,int],NDArray]:
         """Load the data."""
-        pass
+        embeddings_dict:Dict[Tuple[int,int],NDArray] = {}
+        specific_dir: Path = self.config.embeddings_dir / self.config.dataset_name
+        assert specific_dir.exists(), f"{specific_dir} does not exist." 
+        for file_path in specific_dir.glob(f"*.mat"):
+            assert file_path.exists(), f"{file_path} does not exist."
+            user_id, sample_id = map(int, file_path.stem.split('_'))
+            data = sp.io.loadmat(file_path)["Ftemplate"]
+            data = np.squeeze(data)
+            embeddings_dict[(user_id, sample_id)] = data
+        return embeddings_dict
+        
