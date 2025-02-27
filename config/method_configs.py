77d8e3a0ad3773fb80d04772c83b279abb4b2310
futurecodes <15356303+futurecodes@user.noreply.gitee.com>
2025-02-27 11:33:36 +0800

    feat(config): 更新汇总方法配置，添加了对应的verify类型

--- config/method_configs.py
+++ config/method_configs.py
@@ -1,15 +1,19 @@
-from typing import Dict,List
+from typing import Dict,List,Tuple
 from method.base_method import MethodConfig
 from method.bio_hash import BioHashConfig
 from method.bi_avet import BiAVETConfig
 from method.in_avet import InAVETConfig
 from method.swg import SWGConfig
+from verification.verify import VerifierConfig
 
 
 method_names: List[str] = ["BioHash","AVET","Bi_AVET","In_AVET","C_IOM"]
 
-METHOD_CONFIGS: Dict[str,MethodConfig] = {
-    "BioHash":BioHashConfig(),
-    "BiAVET":BiAVETConfig(),
-    "InAVET":InAVETConfig(),
-    "SWG":SWGConfig()}
+METHOD_CONFIGS: Dict[str,Tuple[MethodConfig,VerifierConfig]] = {
+    "SWG": (SWGConfig(),VerifierConfig(measure="hamming")),
+    "BioHash": (BioHashConfig(),VerifierConfig(measure="hamming")),
+    "AVET": (SWGConfig(),VerifierConfig(measure="cosine")),
+    "Bi_AVET": (BiAVETConfig(),VerifierConfig(measure="hamming")),
+    "In_AVET": (InAVETConfig(),VerifierConfig(measure="jaccard")),
+    "C_IOM": (SWGConfig(),VerifierConfig(measure="cosine"))
+}
