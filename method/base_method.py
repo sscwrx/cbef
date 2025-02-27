fa0f67d964964e6027bf8ac8ced43a8cbb904892
futurecodes <15356303+futurecodes@user.noreply.gitee.com>
2025-02-27 10:13:58 +0800

    feat(base_method): 为 MethodConfig 添加 seed 属性并增加 set_seed 方法

--- method/base_method.py
+++ method/base_method.py
@@ -8,7 +8,7 @@ from config.base_config import BaseConfig
 
 class MethodConfig(BaseConfig):
     method_name:Literal["BioHash","AVET","Bi_AVET","In_AVET","C_IOM"]
-
+    seed:int = 1
     
 class BaseMethod(ABC):
         
@@ -19,10 +19,13 @@ class BaseMethod(ABC):
     def __init__(self, config: MethodConfig,**kwargs):
         self.config = config
         self.kwargs = kwargs
+        
+    def set_seed(self,seed:int):
+        self.config.seed = seed
+
 
     @abstractmethod
     def process_feature(self, feature_vector:np.ndarray,seed:int=1) -> np.ndarray:
         """Abstract method to be implemented by subclasses."""
         pass
-
 
